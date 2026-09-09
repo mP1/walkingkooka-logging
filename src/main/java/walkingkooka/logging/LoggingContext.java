cc7e02d8959eeb005525aa2a13ac514c10a76948
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-09 16:51:35 +1000

    LoggingContext

---
+++ src/main/java/walkingkooka/logging/LoggingContext.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2026 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.logging;
+
+import walkingkooka.Context;
+
+public interface LoggingContext extends Context {
+
+    void debug(final String message);
+
+    void debug(final String message,
+               final Throwable throwable);
+
+    void info(final String message);
+
+    void info(final String message,
+              final Throwable throwable);
+
+    void warn(final String message);
+
+    void warn(final String message,
+              final Throwable throwable);
+
+    void error(final String message);
+
+    void error(final String message,
+               final Throwable throwable);
+
+    void log(final LoggingLevel level,
+             final String message);
+
+    boolean isDebugEnabled();
+
+    boolean isInfoEnabled();
+
+    boolean isWarnEnabled();
+
+    boolean isErrorEnabled();
+
+    boolean isNoneEnabled();
+
+    LoggingLevel loggingLevel();
+}
